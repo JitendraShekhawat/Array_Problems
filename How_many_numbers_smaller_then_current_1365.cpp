5b363fe8bf7061dc384b9a9d8b8041b553b0fd2a
JitendraShekhawat <jitendrasinghshekhawat666@gmail.com>
2020-07-13 12:28:55 +0530

    Commit

--- How_many_numbers_smaller_then_current_1365.cpp
+++ How_many_numbers_smaller_then_current_1365.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 //method 1
+//time complexity O(n^2)
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
@@ -22,3 +24,33 @@ public:
     }
 };
 
+
+//Method 2 
+//time complexity O(n)
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        
+        vector<int> a(101, 0);
+        vector<int> b(101, 0);
+        vector<int> ans(nums.size() ,0);
+        //making the frequency array
+        for(int i = 0;i<nums.size();i++){
+            a[nums[i]]++;
+        }
+        //fillig the prefix sum
+        for (int i =0;i<101;i++){
+            if(i == 0 ){
+                b[i] = 0;
+            }
+            else{
+                b[i] = b[i-1] + a[i-1];
+            }
+        }
+        for (int i = 0 ;i<nums.size();i++){
+            
+            ans[i] = b[nums[i]];
+        }
+        return ans;
+    }
+};
