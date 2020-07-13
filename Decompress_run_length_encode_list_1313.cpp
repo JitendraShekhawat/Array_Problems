bcd637847114bb11fe466045d4291e745eee04fa
JitendraShekhawat <jitendrasinghshekhawat666@gmail.com>
2020-07-14 04:35:16 +0530

    Commit

--- Decompress_run_length_encode_list_1313.cpp
+++ Decompress_run_length_encode_list_1313.cpp
@@ -25,3 +25,21 @@ public:
         return ans;
     }
 };
+
+//method 2
+//fater then 10 %
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> ans;
+        
+        for(int i = 0;i<nums.size();i++){
+            if(i%2 == 0){
+                while(nums[i]--){
+                    ans.push_back(nums[i+1]);
+                }
+            }
+        }
+        return ans;
+    }
+};
